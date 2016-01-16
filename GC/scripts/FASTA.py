dcaae2d1165ed6b916129a26822bf6e6924b1dce
Domenico Solazzo <solazzo.domenico@gmail.com>
2016-01-16 23:53:10 +0100

    Adding some FASTA algorithm

---
+++ GC/scripts/FASTA.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+'''A ROSALIND bioinformatics script to extract sequence information FASTA format data.'''
+
+import urllib
+import contextlib
+
+def ReadFASTA(data_location):
+        '''Determines the data type of the FASTA format data and passes the appropriate information to be parsed.'''
+
+        # If given a list, return fasta information from all items in the list.
+        if type(data_location) == list:
+                fasta_list =[]
+                for location in data_location:
+                        fasta_list+=ReadFASTA(location)
+                return fasta_list
+
+
+        # Check for a text file, return fasta info from the text file.
+        if data_location[-4:] == '.txt':
+                with open(data_location) as f:
+                        return ParseFASTA(f)
+
+        # Check for a website, return fasta info from the website.
+        elif data_location[0:4] == 'http':
+                with contextlib.closing(urllib.urlopen(data_location)) as f:
+                        return ParseFASTA(f)
